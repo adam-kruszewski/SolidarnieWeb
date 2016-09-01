d3e05671de688204bb01cf8bb892c66866d5ee03
adam-kruszewski <kruchy.jr@wp.pl>
2016-09-01 21:17:37 +0200

    Przeniesienie tworzenia bazy danych do controllera administracja

---
+++ SolidarnieWebApp/Controllers/AdministracjaController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+using Kruchy.Uzytkownicy.Konfiguracja;
+using NHibernate.Tool.hbm2ddl;
+
+namespace SolidarnieWebApp.Controllers
+{
+    public class AdministracjaController : Controller
+    {
+        private readonly IHibernateSessionProvider sessionProvider;
+
+        public AdministracjaController(
+            IHibernateSessionProvider sessionProvider)
+        {
+            this.sessionProvider = sessionProvider;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult UtworzBaze()
+        {
+            var konfiguracja = sessionProvider.DajKonfiguracje();
+
+            new SchemaExport(konfiguracja).Execute(true, true, false);
+
+            ViewBag.Komunikat = "Utworzono bazę danych";
+            return View();
+        }
+
+    }
+}
