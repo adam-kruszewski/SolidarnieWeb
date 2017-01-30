af5f110b96515202588c28c1d3e42d541df517fd
adam-kruszewski <kruchy.jr@wp.pl>
2017-01-30 15:31:26 +0100

    Przygotowanie do walidowania w testach

---
+++ Kruchy.Testy/Walidacja/MockListeneraWalidacji.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Kruchy.Model.DataTypes.Walidacja;
+
+namespace Kruchy.Testy.Walidacja
+{
+    public class MockListeneraWalidacji : IWalidacjaListener
+    {
+        public List<Tuple<string, string>> Bledy { get; private set; }
+        public List<Tuple<string, string>> Ostrzezenia { get; private set; }
+
+        public MockListeneraWalidacji()
+        {
+            Bledy = new List<Tuple<string, string>>();
+            Ostrzezenia = new List<Tuple<string, string>>();
+        }
+
+        public void Blad(string komunikat, string wlasciwosc)
+        {
+            Bledy.Add(new Tuple<string, string>(komunikat, wlasciwosc));
+        }
+
+        public void Ostrzezenie(string komunikat, string wlasciwosc)
+        {
+            Ostrzezenia.Add(new Tuple<string, string>(komunikat, wlasciwosc));
+        }
+    }
+}
