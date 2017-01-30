de4ec2fe087c3c8aa2fedd0f64cc1d8829c86a0d
adam-kruszewski <kruchy.jr@wp.pl>
2017-01-30 15:32:17 +0100

    Rozszerzenie zwracające listenera połączonego z interfejsem użytkownika

---
+++ SolidarnieWebApp/Walidacja/ControllerExtension.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+using Kruchy.Model.DataTypes.Walidacja;
+
+namespace SolidarnieWebApp.Walidacja
+{
+    public static class ControllerExtension
+    {
+        public static IWalidacjaListener DajListeneraWalidacji(
+            this Controller controller)
+        {
+            return new WalidacjaListenerDlaModelState(controller.ModelState);
+        }
+
+        private class WalidacjaListenerDlaModelState : IWalidacjaListener
+        {
+            private readonly ModelStateDictionary modelStateDictionary;
+
+            public WalidacjaListenerDlaModelState(
+                ModelStateDictionary modelStateDictionary)
+            {
+                this.modelStateDictionary = modelStateDictionary;
+            }
+
+            public void Blad(string komunikat, string wlasciwosc)
+            {
+                modelStateDictionary.AddModelError(wlasciwosc, komunikat);
+            }
+
+            public void Ostrzezenie(string komunikat, string wlasciwosc)
+            {
+            }
+        }
+    }
+}
