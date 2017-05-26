8839a9dc2e1b43c6bf1191ced3f9e2863aa331ca
adam-kruszewski <kruchy.jr@wp.pl>
2017-05-26 13:57:29 +0200

    Dodanie klasy repozytorium NHinbernate

---
+++ Kruchy.NHibernate/Repositories/NHibernateRepository.cs
@@ -0,0 +1,33 @@
+﻿using Kruchy.NHibernate.Provider;
+
+namespace Kruchy.NHibernate.Repositories
+{
+    public class NHibernateRepository<T>
+    {
+        private readonly IHibernateSessionProvider sessionProvider;
+
+        public NHibernateRepository(
+            IHibernateSessionProvider sessionProvider)
+        {
+            this.sessionProvider = sessionProvider;
+        }
+
+        public virtual T Get(object id)
+        {
+            var session = sessionProvider.DajSesje();
+            return session.Get<T>(id);
+        }
+
+        public virtual object Save(object o)
+        {
+            var session = sessionProvider.DajSesje();
+            return session.Save(o);
+        }
+
+        public virtual void Update(object o)
+        {
+            var session = sessionProvider.DajSesje();
+            session.Update(o);
+        }
+    }
+}
