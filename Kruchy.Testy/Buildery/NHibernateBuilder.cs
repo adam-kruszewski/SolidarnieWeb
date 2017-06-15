60894fce89aaf60752b66121a4bf900aa3e50448
adam-kruszewski <kruchy.jr@wp.pl>
2017-06-15 21:25:05 +0200

    Dodaniebuildera i assercji walidacji

---
+++ Kruchy.Testy/Buildery/NHibernateBuilder.cs
@@ -0,0 +1,33 @@
+﻿using Kruchy.NHibernate.Provider;
+using Kruchy.NInject.Adapter.Testy;
+
+namespace Kruchy.Testy.Buildery
+{
+    public class NHibernateBuilder<T>
+        where T : new()
+    {
+        protected T Obiekt;
+
+        public NHibernateBuilder()
+        {
+            Obiekt = new T();
+        }
+
+        public T Build()
+        {
+            return Obiekt;
+        }
+
+        public T Save()
+        {
+            var obiektDoZapisania = Build();
+
+            var sessionProvider =
+                Injector.Instancja.Pobierz<IHibernateSessionProvider>();
+
+            sessionProvider.DajSesje().Save(obiektDoZapisania);
+
+            return obiektDoZapisania;
+        }
+    }
+}
---
+++ Kruchy.Testy/Walidacja/MockListeneraWalidacjiExtension.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using FluentAssertions;
+
+namespace Kruchy.Testy.Walidacja
+{
+    public static class MockListeneraWalidacjiExtension
+    {
+        public static void ZawieraBlad(
+            this MockListeneraWalidacji listener,
+            string komunikat)
+        {
+            listener.Bledy.SingleOrDefault(o => o.Item1 == komunikat)
+                .Should().NotBeNull(string.Format("Brak błędu: \"{0}\"", komunikat));
+        }
+
+        public static void ZawieraBladDlaWlasciwosci(
+            this MockListeneraWalidacji listener,
+            string komunikat,
+            string wlasciwosc)
+        {
+            listener
+                .Bledy
+                .SingleOrDefault(o => o.Item1 == komunikat && o.Item2 == wlasciwosc)
+                .Should().NotBeNull(string.Format(
+                    "Brak błędu \"{0}\" dla właściwości \"{1}\"", komunikat, wlasciwosc));
+        }
+
+        public static void NieZawieraBledu(this MockListeneraWalidacji listener)
+        {
+            listener.Bledy.Should().BeEmpty();
+        }
+    }
+}
