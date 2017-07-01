80af0426217fd7624715d551a2aed2a6bf0c90fd
adam-kruszewski <kruchy.jr@wp.pl>
2017-07-01 14:58:14 +0200

    Poprawa formatowania.

--- Kruchy.Zakupy/Services/Impl/DefinicjeZamowieniaService.cs
+++ Kruchy.Zakupy/Services/Impl/DefinicjeZamowieniaService.cs
@@ -1,10 +1,7 @@
-﻿using Kruchy.NHibernate.Repositories;
-using Kruchy.Zakupy.Domain;
-namespace Kruchy.Zakupy.Services.Impl
+﻿namespace Kruchy.Zakupy.Services.Impl
 {
     class DefinicjeZamowieniaService : IDefinicjeZamowieniaService
     {
-
         public int Wstaw(WstawienieDefinicjiZamowieniaRequest request)
         {
             throw new System.NotImplementedException();
