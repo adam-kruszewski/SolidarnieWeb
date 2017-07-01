b50fd3102953e196317afde7a4435d6b08dd5d72
adam-kruszewski <kruchy.jr@wp.pl>
2017-07-01 14:07:34 +0200

    Dodanie modułu na zakupy/

---
+++ Kruchy.Zakupy/Domain/DefinicjaZamowienia.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Kruchy.Zakupy.Domain
+{
+    public class DefinicjaZamowienia
+    {
+        public virtual int ID { get; set; }
+
+        public virtual string Nazwa { get; set; }
+
+        public virtual DateTime CzasKoncaZamawiania { get; set; }
+
+        public virtual bool Zamkniete { get; set; }
+    }
+}
---
+++ Kruchy.Zakupy/ZakupyModule.cs
@@ -0,0 +1,13 @@
+﻿using Kruchy.NHibernate.RejestracjaAssembly;
+using Ninject.Modules;
+
+namespace Kruchy.Zakupy
+{
+    public class ZakupyModule : NinjectModule
+    {
+        public override void Load()
+        {
+            RejestratorAssembly.Zarejestruj(this.GetType().Assembly);
+        }
+    }
+}
