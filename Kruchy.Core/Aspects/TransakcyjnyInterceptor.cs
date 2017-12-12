98338d99898b7d2ca3160106d496edfb7cb90414
adam-kruszewski <kruchy.jr@wp.pl>
2017-12-12 21:34:16 +0100

    Dodaję moduł do aspektów i innych niskopoziomowych rzeczy.

---
+++ Kruchy.Core/Aspects/TransakcyjnyInterceptor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Kruchy.NHibernate.Provider;
+using Ninject.Extensions.Interception;
+
+namespace Kruchy.Core.Aspects
+{
+    public class TransakcyjnyInterceptor : IInterceptor
+    {
+        private readonly IHibernateSessionProvider sessionProvider;
+
+        public TransakcyjnyInterceptor(
+            IHibernateSessionProvider sessionProvider)
+        {
+            this.sessionProvider = sessionProvider;
+        }
+
+        public void Intercept(IInvocation invocation)
+        {
+            try
+            {
+                var bylaTransakcja = sessionProvider.JestOtwartaTransakcja();
+                if (!bylaTransakcja)
+                    sessionProvider.RozpocznijTransakcje();
+                invocation.Proceed();
+                if (!bylaTransakcja)
+                    sessionProvider.Commit();
+            }
+            catch (Exception)
+            {
+                if (sessionProvider.JestOtwartaTransakcja())
+                    sessionProvider.Rollback();
+                throw;
+            }
+        }
+    }
+}
