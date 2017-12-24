8c2d82dcc1588fbcdcba0c99ad6c91959d73736c
adam-kruszewski <kruchy.jr@wp.pl>
2017-12-24 13:30:48 +0100

    Metoda rozszerzająca dla mapowania.

---
+++ Kruchy.Core/Mapowanie/MaperExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Kruchy.Core.Mapowanie
+{
+    public static class MaperExtensions
+    {
+        public static T Mapuj<T>(this object zrodlo)
+        {
+            return new Maper().Mapuj<T>(zrodlo);
+        }
+    }
+}
