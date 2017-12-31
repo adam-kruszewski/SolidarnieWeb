7cb7e16b49c9ade1052b0ce57c5ca4dc2a13047e
adam-kruszewski <kruchy.jr@wp.pl>
2017-12-31 14:38:50 +0100

    Logowanie startu aplikacji oraz zapytań nhibernate

--- SolidarnieWebApp/Global.asax.cs
+++ SolidarnieWebApp/Global.asax.cs
@@ -1,6 +1,9 @@
-﻿using System.Web.Mvc;
+﻿using System.IO;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
 
 namespace SolidarnieWebApp
 {
@@ -12,6 +15,15 @@ namespace SolidarnieWebApp
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var log4netConfigFile = Server.MapPath("~/log4net.config");
+            log4net.Config.XmlConfigurator.ConfigureAndWatch(new FileInfo(log4netConfigFile));
+            
+
+            log4net.ILog log = log4net.LogManager.GetLogger
+                (typeof(MvcApplication));
+
+            log.Info("Start aplikacji");
         }
     }
 }
