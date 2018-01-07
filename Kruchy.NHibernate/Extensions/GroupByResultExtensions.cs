6280794a0af5c0c8d22b095ff31f0ab0a36fda76
adam-kruszewski <kruchy.jr@wp.pl>
2018-01-07 14:20:37 +0100

    Dodaję extension metod do rzutowania wyniku group by w zapytaniu z Nhibernate.

---
+++ Kruchy.NHibernate/Extensions/GroupByResultExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Kruchy.NHibernate.Extensions
+{
+    public static class GroupByResultExtensions
+    {
+        public static T To<T>(this object[] value)
+        {
+            var result =
+                (T)typeof(T).GetConstructor(new Type[0]).Invoke(new object[0]);
+
+            var properties = typeof(T).GetProperties();
+
+            for (int i = 0; i < Math.Min(properties.Length, value.Length); i++)
+            {
+                properties[i].SetValue(result, value[i]);
+            }
+
+            return result;
+        }
+    }
+}
