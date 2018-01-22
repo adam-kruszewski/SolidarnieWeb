87a54b8c2748ad49804efbb01cc3a614691a3b52
adam-kruszewski <kruchy.jr@wp.pl>
2018-01-22 20:30:08 +0100

    Dodanie tabeli na uprawnienia użytkownika.

---
+++ Kruchy.Uzytkownicy/Domain/UprawnienieUzytkownika.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Kruchy.Uzytkownicy.Domain
+{
+    public class UprawnienieUzytkownika
+    {
+        public virtual int ID { get; set; }
+
+        public virtual DateTime DataOd { get; set; }
+
+        public virtual DateTime? DataDo { get; set; }
+
+        public virtual string Uprawnienie { get; set; }
+
+        public virtual Uzytkownik Uzytkownik { get; set; }
+    }
+}
