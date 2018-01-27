2a2ff4072292c48e2642d7dd9cf46ae837e923e5
adam-kruszewski <kruchy.jr@wp.pl>
2018-01-27 12:48:49 +0100

    Przygotowanie do obsługi uprawnień na poziomie serwisów.

---
+++ Kruchy.Uzytkownicy/Aspects/WymagaUprawnieniaAttribute.cs
@@ -0,0 +1,24 @@
+﻿using Ninject;
+using Ninject.Extensions.Interception;
+using Ninject.Extensions.Interception.Attributes;
+using Ninject.Extensions.Interception.Request;
+
+namespace Kruchy.Uzytkownicy.Aspects
+{
+    public class WymagaUprawnieniaAttribute : InterceptAttribute
+    {
+        public string Uprawnienie { get; set; }
+
+        public WymagaUprawnieniaAttribute(string uprawnienie)
+        {
+            Uprawnienie = uprawnienie;
+        }
+
+        public override IInterceptor CreateInterceptor(IProxyRequest request)
+        {
+            var interceptor = request.Kernel.Get<WymagaUprawnieniaInterceptor>();
+            interceptor.ustawUprawnienie(Uprawnienie);
+            return interceptor;
+        }
+    }
+}
---
+++ Kruchy.Uzytkownicy/Aspects/WymagaUprawnieniaInterceptor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Kruchy.Core.Autentykacja;
+using Kruchy.Uzytkownicy.Uprawnienia;
+using Ninject.Extensions.Interception;
+
+namespace Kruchy.Uzytkownicy.Aspects
+{
+    public class WymagaUprawnieniaInterceptor : IInterceptor
+    {
+        private readonly IUprawnieniaService uprawnieniaService;
+        private readonly IUzytkownikProvider uzytkownikProvider;
+
+        private string uprawnienie;
+
+        public WymagaUprawnieniaInterceptor(
+            IUprawnieniaService uprawnieniaService,
+            IUzytkownikProvider uzytkownikProvider)
+        {
+            this.uprawnieniaService = uprawnieniaService;
+            this.uzytkownikProvider = uzytkownikProvider;
+        }
+
+        public void ustawUprawnienie(string uprawnienie)
+        {
+            this.uprawnienie = uprawnienie;
+        }
+
+        public void Intercept(IInvocation invocation)
+        {
+            var uzytkownikID = uzytkownikProvider.DajZalogowanego().ID;
+
+            if (!uprawnieniaService.SprawdzCzyPosiada(uzytkownikID, uprawnienie))
+                throw new ApplicationException(string.Format("Brak uprawnienia {0}.", uprawnienie));
+
+            invocation.Proceed();
+        }
+    }
+}
